172b33b2acf0c9e9a2a3a7638c34422f2738deb9
VABMI <vaxoalavidze97@gmail.com>
2018-11-29 14:36:04 +0400

    X

--- primeNumbers/primeVisual/delete/delete.cpp
+++ primeNumbers/primeVisual/delete/delete.cpp
@@ -1,50 +1,112 @@
-#include <windows.h>
-#include <stdio.h>
 
 #include "stdafx.h"
 HFONT hfont_global;
-
-
+HWND globh;
+		  static char buf2[1000000];
 
 //----------------------------------------------------------------
 
-DWORD WINAPI Thread_no_1( void* bufer1 ) 
+DWORD WINAPI Thread_no_1( void** bufer1 ) 
 {
-	char*bufer=(char*)bufer1;
 	
-		MessageBox(0,(char*)bufer1,(char*)bufer1,0);
-				MessageBox(0,(char*)bufer,bufer,0);
-
-		        int length=strlen((char*)bufer);
-				char *number=(char*)malloc(length);//=bufer;
-				strcpy(number,(char*)bufer);
-			   int x=atoi(number);
+			   int x=(int)bufer1[0];
 
 
-			   char buf2[1000];
-			   int i=3;
-
-				for( i=3;i<=x;i+=2)
+	
+			
+			  /*
+				for(int i=3;i<=(int)bufer1[0];i+=2)
 					{
 						if(i%3!=0)
 						{
-							char *ff;
-					
 							char buffer[20];
 							itoa(i,buffer,10);
-				
-
 							strcat(buf2,(char*)buffer);
 							strcat(buf2,"\n\r");
 						}
 					}
+					*/
 
-				MessageBox(0,buf2,buf2,0);
+			  
+					  for(int i=2;i<=(int)bufer1[0];i++)
+						{
+							for(int j=2;j<i;j++)
+							{
+								if(i%j==0)
+									break;
+								else if(i==j+1)
+								{
+									char buffer[2000];
+
+									sprintf(buffer,"%i",i);
+								//	itoa(i,buffer,90);
+									strcat(buf2,(char*)buffer);
+									//strcat(buf2,"\n\r");
+									strcat(buf2,"|_| ");
+							
+								
+								}
+							}
+						}
+
 				
-				free(number);
+					
+
+
+
+					//	MessageBox(0,buf2,buf2,MB_TOPMOST);	
+						
+						
 
 	return 1; 
 } 
+DWORD WINAPI Thread_no_2( void** bufer1 ) 
+{
+	
+			   int x=atoi((char*)bufer1);
+
+
+			  static char buf2[1000];
+			
+			  /*
+				for(int i=(int)bufer1[0];i<=(int)bufer1[1];i+=2)
+					{
+
+						for(int h=1;h<=10;h++)
+						{
+								if(i%h>)
+								{
+									char buffer[20];
+									itoa(i,buffer,10);
+									strcat(buf2,(char*)buffer);
+									strcat(buf2,"\n\r");
+								}
+						}
+					}
+					*/
+
+
+
+					  for(int i=(int)bufer1[0];i<=(int)bufer1[1];i++)
+						{
+							for(int j=2;j<i;j++)
+							{
+								if(i%j==0)
+									break;
+									else if(i==j+1)
+								{
+									char buffer[20];
+									itoa(i,buffer,10);
+									strcat(buf2,(char*)buffer);
+									strcat(buf2,"\n\r");
+								}
+						}
+					  }
+						MessageBox(0,buf2,buf2,0);
+	return 1; 
+} 
+
+
 //----------------------------------------------------------------
 long __stdcall window_main_function_chvenia(HWND hwnd,unsigned int message
 					, unsigned int wparam,long lparam)
@@ -53,12 +115,14 @@ long __stdcall window_main_function_chvenia(HWND hwnd,unsigned int message
 	{
 		case WM_CREATE:
 			{
+
+				globh=hwnd;
 				RECT r;
 				CreateWindow("edit","Main",WS_CHILD|WS_BORDER|WS_VISIBLE,0,0,150,30,hwnd,(HMENU)1,0,0);
 
 				CreateWindow("Button","gashveba",WS_CHILD|WS_BORDER|WS_VISIBLE,155,0,80,30,hwnd,(HMENU)2,0,0);
 				GetClientRect(hwnd,&r);
-				CreateWindow("edit","Main",WS_CHILD|WS_BORDER|WS_VISIBLE,0,50,r.right,r.bottom-55,hwnd,(HMENU)1,0,0);
+			globh=CreateWindow("edit","",WS_CHILD|WS_BORDER|WS_VISIBLE|WS_VSCROLL|WS_HSCROLL|ES_MULTILINE,0,50,r.right,r.bottom-55,hwnd,(HMENU)3,0,0);
 
 			}
 		break;
@@ -67,13 +131,34 @@ long __stdcall window_main_function_chvenia(HWND hwnd,unsigned int message
 			{
 
 				if(wparam==2)
-				{
+				{	
+					
+					
+					
+					
+					
+					static int arr[100];
 					char bufer[100];
 					SendMessage(GetDlgItem(hwnd,1),WM_GETTEXT,100,(LPARAM)bufer);
-					MessageBox(0,bufer,bufer,0);
+					//MessageBox(0,bufer,bufer,0);
+					int value=atoi(bufer);
+					arr[0]=(value/2);
+					arr[1]=value;
+					arr[2]=(int)hwnd;
+			//		int val1=(value/2);
 
-					CreateThread(0,0,Thread_no_1,&bufer,0,0);
+					HANDLE Thread1=CreateThread(0,0,(LPTHREAD_START_ROUTINE)Thread_no_1,&arr,0,0);
 
+					WaitForSingleObject(Thread1,INFINITE);
+						SendMessage(globh,EM_REPLACESEL,strlen(buf2),(LPARAM)buf2);
+
+				//	HANDLE Thread2=CreateThread(0,0,(LPTHREAD_START_ROUTINE)Thread_no_2,&arr,0,0);
+				
+
+				//	system("pause");
+				MessageBox(0,"gacdaaaa",bufer,0);
+
+					system("pause");
 				}
 
 			
--- primeNumbers/primeVisual/delete/stdafx.h
+++ primeNumbers/primeVisual/delete/stdafx.h
@@ -10,7 +10,9 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
-
+#include <windows.h>
+#include <stdio.h>
+#include<stdlib.h>
 // C RunTime Header Files
 #include <stdlib.h>
 #include <malloc.h>
